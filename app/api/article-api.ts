9322b831c48504eac38b221bc949d12ae596bca6
madidev <madifrontend@gmail.com>
2024-10-16 17:33:39 +0330

    add article dynamic page

--- app/api/article-api.ts
+++ app/api/article-api.ts
@@ -19,4 +19,12 @@ export class ArticleApi {
       ).json()) as ArticleListResponse
     ).results;
   }
+  static async fetchByTitle(title: string): Promise<Article> {
+    const formatedTitle = title.replace("/:/g", "");
+    return (
+      (await (
+        await fetch(`${process.env.API_BASE_URL}?title=${formatedTitle}`)
+      ).json()) as ArticleListResponse
+    ).results[0];
+  }
 }
