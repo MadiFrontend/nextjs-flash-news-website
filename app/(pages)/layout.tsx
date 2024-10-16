5a923266ab7c2cbf8fb39979784c0564e4f71ef1
madidev <madifrontend@gmail.com>
2024-10-16 19:12:16 +0330

    add clock comp(client) without ssr

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,4 +1,10 @@
 import { Nav } from "@/app/components/Nav/Nav";
+import Clock from "../components/Clock/Clock";
+import dynamic from "next/dynamic";
+
+const ClockNoSSR = dynamic(() => import("@/app/components/Clock/Clock"), {
+  ssr: false,
+});
 
 export default function PagesLayout({
   children,
@@ -7,6 +13,7 @@ export default function PagesLayout({
 }) {
   return (
     <div className="p-8">
+      <ClockNoSSR />
       <div className="flex ">
         <Nav />
         <div className="mt-16 pl-8 w-full">{children}</div>
